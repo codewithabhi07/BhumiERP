2c02c7f4e4ca91272a6a7863f104b95af7af3987
Code_With_Abhi <106855724+codewithabhi07@users.noreply.github.com>
2026-04-18 20:30:15 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/billing',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
